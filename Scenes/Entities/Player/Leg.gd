d4a6839b040de8d23fcfa8c5fe47d0d467b2b8cf
Artur Szałyga <arturszalyga@wp.pl>
2021-11-05 00:41:43 +0100

    Movement adjusted, legs WIP

--- Scenes/Entities/Player/Leg.gd
+++ Scenes/Entities/Player/Leg.gd
@@ -1,16 +1,33 @@
 extends Node2D
 
 onready var wallDetector = get_node("Grabber/WallDetector")
+onready var leg = get_node("Leg")
+onready var fleg = get_node("FLeg")
 onready var grabber = get_node("Grabber")
+onready var player = get_parent()
+
 var closestWall
 var direction = Vector2(0, 0)
+var latched_on = false
 
 func _physics_process(delta):
 	if closestWall != null:
-		direction = (closestWall.position - grabber.position).normalized()
-	grabber.apply_impulse(Vector2(35, 0), direction * 100)
-
+		direction = position.direction_to(closestWall.position)
+	if !latched_on:
+		grabber.set_applied_force(direction * 1000)
 
 func _on_WallDetector_body_entered(body):
-	if body.get_collision_layer() == 3:
+	if body.get_collision_layer() == 4:
 		closestWall = body
+
+
+func _on_Grabber_body_entered(body):
+	grabber.sleeping = true
+	grabber.mode = RigidBody2D.MODE_STATIC
+	latched_on = true
+
+
+func _on_Grabber_body_exited(body):
+	grabber.mode = RigidBody2D.MODE_RIGID
+	grabber.apply_central_impulse(Vector2.ONE)
+	latched_on = false
--- Scenes/Entities/Player/Player.gd
+++ Scenes/Entities/Player/Player.gd
@@ -11,5 +11,5 @@ func _physics_process(delta):
 
 func movement(delta):
 	direction = Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), Input.get_action_strength("move_down") - Input.get_action_strength("move_up")).normalized()
-	add_force(Vector2(0, 0), direction * speed * mass)
+	set_applied_force(direction * speed * mass * 500)
 	
